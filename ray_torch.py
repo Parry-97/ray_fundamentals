e8b82ebd3c861d96f1689c902b594e5d4c2fc949
Param Pal Singh <singhparampal97@gmail.com>
2025-09-18 21:35:41 +0200

    ray train with torch

--- ray_torch.py
+++ ray_torch.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import datetime
+from torchvision.datasets.celeba import csv
+from torchvision.transforms import Compose, Normalize, ToTensor
 import tqdm
 import torch
 from torchvision.models import resnet18
@@ -8,7 +11,6 @@ from torch.optim import Adam
 
 
 def train_loop_torch(
-    dataloader: torch.utils.data.DataLoader,
     num_epochs: int = 2,
     batch_size: int = 32,
     local_path: str = "./checkpoints",
@@ -18,9 +20,10 @@ def train_loop_torch(
 
     criterion = CrossEntropyLoss()
     model = load_model_torch().to(device)
+    dataloader = build_data_loader_torch(batch_size=batch_size)
     optimizer = Adam(model.parameters(), lr=1e-5)
 
-    for epoch in tqdm.tqdm(range(num_epochs), desc="Training"):
+    for epoch in tqdm.tqdm(range(num_epochs), desc="Epochs", leave=False):
         epoch_loss = 0
         for images, labels in tqdm.tqdm(
             dataloader, desc=f"Epoch {epoch + 1}/{num_epochs}", leave=False
@@ -35,7 +38,11 @@ def train_loop_torch(
 
             epoch_loss += loss.item()
         avg_loss = epoch_loss / len(dataloader)
-        print(f"Epoch {epoch + 1} average loss: {avg_loss}")
+        metrics = report_metrics_torch(loss=avg_loss, epoch=epoch)
+        Path(local_path).mkdir(parents=True, exist_ok=True)
+        save_checkpoint_and_metrics_torch(
+            model=model, metrics=metrics, epoch=epoch, local_path=local_path
+        )
 
 
 def load_model_torch() -> torch.nn.Module:
@@ -58,4 +65,40 @@ def build_resnet18() -> torch.nn.Module:
     return model
 
 
-dataset = MNIST(root="./data", train=True, download=True)
+def build_data_loader_torch(batch_size: int = 32) -> torch.utils.data.DataLoader:
+    # NOTE: Composes several transforms together.
+    transform = Compose([ToTensor(), Normalize((0.5,), (0.5,))])
+    dataset = MNIST(
+        root="./data",
+        train=True,
+        download=True,
+        transform=transform,
+    )
+    train_loader = torch.utils.data.DataLoader(
+        dataset=dataset, batch_size=batch_size, shuffle=True, drop_last=True
+    )
+
+    return train_loader
+
+
+def report_metrics_torch(loss: float, epoch: int) -> dict:
+    metrics = {"loss": loss, "epoch": epoch}
+    print(f"\nEpoch {epoch} average loss: {loss}")
+    return metrics
+
+
+def save_checkpoint_and_metrics_torch(
+    model: torch.nn.Module, metrics: dict, epoch: int, local_path: str
+) -> None:
+    with open(Path(local_path) / "metrics.csv", "a") as f:
+        writer = csv.writer(f)
+        writer.writerow(metrics.values())
+    checkpoint_path = Path(local_path) / "model.pth"
+    torch.save(model.state_dict(), checkpoint_path)
+
+
+timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+storage_folder = "./cluster_storage"
+local_path = f"{storage_folder}/torch-{timestamp}/"
+
+train_loop_torch(num_epochs=1, local_path=local_path)
